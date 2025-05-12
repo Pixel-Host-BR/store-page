61fa3691e943d3aa4c010425dec95848103e15df
snoop <codificarnet@gmail.com>
2025-05-12 08:32:12 -0300

    commit--10

--- js/script.js
+++ js/script.js
@@ -25,3 +25,13 @@ mode.addEventListener('click', () => {
   }
 });
 
+// Start of Tawk.to Script
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function() {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/6821b4c85fa91f190c49b7a7/1ir1qkclc';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
